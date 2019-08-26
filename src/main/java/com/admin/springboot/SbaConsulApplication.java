13402577a057572f1d4d9fa32b7f85e4e8d72550
weihua <weihua@springboot>
2019-08-26 17:36:16 +0800

    2019-8-26

---
+++ src/main/java/com/admin/springboot/SbaConsulApplication.java
@@ -0,0 +1,16 @@
+package com.admin.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableAdminServer
+public class SbaConsulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbaConsulApplication.class, args);
+	}
+
+}
