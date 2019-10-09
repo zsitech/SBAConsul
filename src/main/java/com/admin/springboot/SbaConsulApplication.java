a6433ab29c9f6cb091c8097077cac4921119ddd4
weihua <weihua@springboot>
2019-10-09 15:07:26 +0800

    2019

--- src/main/java/com/admin/springboot/SbaConsulApplication.java
+++ src/main/java/com/admin/springboot/SbaConsulApplication.java
@@ -2,7 +2,6 @@ package com.admin.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication
